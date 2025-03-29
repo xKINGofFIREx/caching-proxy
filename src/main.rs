8a36347c477119f3bddd450c7b388d39dfb27fd8
Mavis <artem.ilin.03@mail.ru>
2025-03-29 19:36:19 +0300

    add arg matching

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
 
-    match args {
+    match args.as_slice() {
+        [port_arg, port, origin_arg, origin] if port_arg == "--port" && origin_arg == "--origin" => ,
+        [clear_cache_arg] if clear_cache_arg == "--clear-cache" => ,
         _ => println!("Wrong arguments"),
     }
 }
