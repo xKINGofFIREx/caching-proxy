02122f03b129dce0ee717e1654c451cfd4e219dd
xKINGofFIREx <artemilin270703@gmail.com>
2025-04-10 00:35:35 +0300

    test commit

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-
+    
+    //parsing args
     match args.as_slice() {
         [port_arg, port, origin_arg, origin] if port_arg == "--port" && origin_arg == "--origin" => ,
         [clear_cache_arg] if clear_cache_arg == "--clear-cache" => ,
